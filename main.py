f0af879aca79f7e84182483b0d7509c18f22c9f0
Janis <janisgoesser92@gmail.com>
2020-05-20 10:44:11 +0200

    Stuff

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import os
+import pickle
+from time import sleep
 from bot import settings
 from bot.get_chromedriver import ChromeUserDir
 # d = ChromeDriverDownload.install_system_specific_driver()
 
-d = settings.get_driver(use_storage=True)
+driver = settings.get_driver(
+    use_storage=True,
+)
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+driver.get("http://www.dropbox.com")
+# sleep(45)
+pickle_file = os.path.join(DIR,'cookies.pkl')
+# pickle.dump( driver.get_cookies() , open(pickle_file,"wb"))
+# driver.quit()
+
+
+# cookies = pickle.load(open(pickle_file, "rb"))
+# for cookie in cookies:
+#     try:
+#         print('YOPE')
+#         driver.add_cookie(cookie)
+#     except:
+#         print('NOPE')
+#         continue
+
+# driver.get("http://www.dropbox.com")
